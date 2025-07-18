59a24104d8554fe9cd780977d62bba0954886eca
Flo1602 <flo16022003@gmail.com>
2025-07-18 17:26:48 +0200

    Implemented Saving Stats after each Learning Session

--- src/pages/learn/WordsLearningMenu.tsx
+++ src/pages/learn/WordsLearningMenu.tsx
@@ -14,7 +14,6 @@ const WordsLearningMenu = () => {
     useEffect(() => {
         getWordProgress().then((res) =>{
             setProgressData(res.data);
-            console.log(res.data);
         })
     }, []);
 
